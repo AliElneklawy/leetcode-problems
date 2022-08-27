c41fa900f041b386e845bd44f44ec7480def3e19
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2022-08-27 16:22:14 +0200

    Update palindrom number.py

--- palindrom number.py
+++ palindrom number.py
@@ -1,8 +1,2 @@
 def isPalindrome(x: int) -> bool:
-    if x < 0:
-        return False
-    n = str(x)
-    rev = n[::-1]
-    if rev == n:
-        return True
-    return False
+        return False if x < 0 else str(x) == str(x)[::-1]
