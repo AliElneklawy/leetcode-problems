7d17729c2dc3842fdda2bcb892cb0a6fdfd602a7
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2022-08-27 17:03:50 +0200

    Add files via upload

---
+++ strinng to integer.py
@@ -0,0 +1,14 @@
+import re
+def myAtoi(s: str) -> int:
+    s = s.strip()
+    try:
+        x = int(''.join((re.findall(r"^[-|+]?\d+", s))))
+        if x < (-2**31):
+            return -2**31
+        if x >= (2**31):
+            return (2**31) - 1
+        return x
+    except:
+        return 0
+
+print(myAtoi("2147483648"))
