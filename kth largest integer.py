a48ac96978fef38f90490d86ee0b5bc88bdf40c6
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2022-08-30 07:32:40 +0200

    Update kth largest integer.py

--- kth largest integer.py
+++ kth largest integer.py
@@ -2,8 +2,5 @@ def kthLargestNumber(nums, k: int) -> str:
         nums = list(map(int, nums))
         nums.sort(reverse=True)
         k-=1
-        while k>=0:
-            if nums.index(nums[k]) == k:
-                return str(nums[k])
-            k-=1
+       return str(nums[k])
 print(kthLargestNumber(["2","21","12","1"], 3))
