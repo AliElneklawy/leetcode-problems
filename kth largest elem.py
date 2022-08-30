6471e0d008da37b6ab98ec490c1292120af9b8fb
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2022-08-30 09:37:52 +0200

    Update kth largest elem.py

--- kth largest elem.py
+++ kth largest elem.py
@@ -1,17 +1,11 @@
 from collections import Counter
-# def isAnagram(s: str, t: str) -> bool:
-#     if len(s) != len(t): return False
-#     freq = Counter(s)
-#     for char in t:
-#         if char in freq and t.count(char) == freq[char]:
-#             valid = True
-#         else:
-#             return False
-#     return valid
-# print(isAnagram("ab", 'a'))
-
-def frequencySort(s: str) -> str:
+def isAnagram(s: str, t: str) -> bool:
+    if len(s) != len(t): return False
     freq = Counter(s)
-    print(freq)
-    return ''.join(sorted(freq.elements()))
-print(frequencySort("treeE"))
+    for char in t:
+        if char in freq and t.count(char) == freq[char]:
+            valid = True
+        else:
+            return False
+    return valid
+print(isAnagram("ab", 'a'))
