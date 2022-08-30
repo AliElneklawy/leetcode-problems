d834216e0cfa29ccfc1d420caabef691ed7abe17
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2022-08-30 07:28:47 +0200

    Add files via upload

---
+++ kth largest elem.py
@@ -0,0 +1,10 @@
+
+def kthLargestNumber(nums, k: int) -> str:
+        nums = list(map(int, nums))
+        nums.sort(reverse=True)
+        k-=1
+        while k>=0:
+            if nums.index(nums[k]) == k:
+                return str(nums[k])
+            k-=1
+print(kthLargestNumber(["2","21","12","1"], 3))
